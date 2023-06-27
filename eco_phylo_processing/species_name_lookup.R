1528b4104930af6db2f768796e52efcfce5c5c4c
Joe Hilton <joe.b.hilton@gmail.com>
2023-06-27 13:20:37 +0100

    Corrections and analysis added to species lookup

--- eco_phylo_processing/species_name_lookup.R
+++ eco_phylo_processing/species_name_lookup.R
@@ -32,12 +32,7 @@ AVONET_conflicts <- distinct(AVONET_conflicts)
 cat("Total number of conflicts is", nrow(AVONET_conflicts))
 
 # Assign ID based on consensus
-get_single_bird_id <- function(binomial_name, priority_list){
-  # priority_list defines the order we should consult the lists if we need to
-  # make an arbitrary decision
-  if (missing(priority_list)){
-    priority_list <- c("eBird", "BirdLife", "BirdTree")
-  }
+get_single_bird_id <- function(binomial_name){
   # Return -100 if provided with na
   if (binomial_name=="na"){
     return("-100")
@@ -58,7 +53,7 @@ get_single_bird_id <- function(binomial_name, priority_list){
   if (binomial_name %in% AVONET_df$Species2_eBird){
     ID_candidates <- append(ID_candidates, AVONET_df$Avibase.ID[
       which(AVONET_df$Species2_eBird==binomial_name)])
-    no_matches <- length(which(AVONET_df$Species1_BirdLife==binomial_name))
+    no_matches <- length(which(AVONET_df$Species2_eBird==binomial_name))
     for (i in 1:no_matches){
       ID_weightings <- append(ID_weightings, 1/no_matches)
       ID_sources <- append(ID_sources, "eBird")
@@ -67,7 +62,7 @@ get_single_bird_id <- function(binomial_name, priority_list){
   if (binomial_name %in% AVONET_df$Species3_BirdTree){
     ID_candidates <- append(ID_candidates, AVONET_df$Avibase.ID[
       which(AVONET_df$Species3_BirdTree==binomial_name)])
-    no_matches <- length(which(AVONET_df$Species1_BirdLife==binomial_name))
+    no_matches <- length(which(AVONET_df$Species3_BirdTree==binomial_name))
     for (i in 1:no_matches){
       ID_weightings <- append(ID_weightings, 1/no_matches)
       ID_sources <- append(ID_sources, "BirdTree")
@@ -88,56 +83,27 @@ get_single_bird_id <- function(binomial_name, priority_list){
     }
     candidate_dist <- c()
     for (i in 1:length(unique_candidates)){
-      cat("i=", i, "\n")
+      # cat("i=", i, "\n")
       candidate_dist <- append(candidate_dist,
                                sum(ID_weightings[
                                  which(ID_candidates==unique_candidates[i])]))
     }
-    cat(unique_candidates, "\n", ID_candidates, "\n", candidate_dist, "\n", ID_weightings, "\n")
-    return(unique_candidates[which.max(candidate_dist)])
+    # cat(unique_candidates, "\n", ID_candidates, "\n", candidate_dist, "\n", ID_weightings, "\n")
+    if (length(which(candidate_dist==max(candidate_dist)))==1){
+      return(unique_candidates[which(candidate_dist==max(candidate_dist))])
+    }
+    else{
+      # cat("Multiple consensus options for species", binomial_name,"\n")
+      return(sample(unique_candidates[which(candidate_dist==max(candidate_dist))], 1))
+    }
   }
-  
-  
-  
-  # else if (length(unique(ID_candidates))==1){
-  #   # If we only have one candidate, return it - note that in this case it
-  #   # doesn't matter how many of the species lists actually contain the
-  #   # requested species.
-  #   return(ID_candidates[1])
-  # }
-  # else if ((length(ID_candidates)==3)&(length(unique(ID_candidates))==2)){
-  #   # If the species appears in all three lists and there is a majority choice,
-  #   # then return that majority choice:
-  #   return(names(sort(table(ID_candidates), decreasing=TRUE))[1])
-  # }
-  # else if ((length(ID_candidates)==2)&(length(unique(ID_candidates))==2)){
-  #   # If there are two candidates identified which differ, which one we return
-  #   # depends on where the candidates' sources sit in the priority list
-  #   if (priority_list[1] %in% ID_sources){
-  #     # If top priority list contains the species, return the ID identified by
-  #     # that list
-  #     return(ID_candidates[which(ID_sources==priority_list[1])])
-  #   }
-  #   else{
-  #     # Otherwise return choice identified by second priority list
-  #     return(ID_candidates[which(ID_sources==priority_list[2])])
-  #   }
-  # }
-  # else if (length(unique(ID_candidates))==3){
-  #   # If we have three different options return the one identified by the top
-  #   # priority list
-  #   return(ID_candidates[which(ID_sources==priority_list[1])])
-  # }
 }
 
-get_bird_ids <- function(name_vect, priority_list){
-  if (missing(priority_list)){
-    priority_list <- c("eBird", "BirdLife", "BirdTree")
-  }
+get_bird_ids <- function(name_vect){
   id_vect <- vector(length = length(name_vect))
   for (n in 1:length(name_vect)){
-    cat("n=",n,"\n")
-    id_vect[n] <- get_single_bird_id(name_vect[n], priority_list)
+    # cat("n=",n,"\n")
+    id_vect[n] <- get_single_bird_id(name_vect[n])
   }
   return(id_vect)
 }
@@ -154,27 +120,57 @@ mismatch_species2 <- AVONET_df$Species2_eBird[list2_mistmatch]
 list3_mistmatch <- which(test_id_list3!=AVONET_df$Avibase.ID)
 mismatch_species3 <- AVONET_df$Species3_BirdTree[list3_mistmatch]
 
-# Try looking at all possible priority lists to see if there's one that gives
-# us fewer mismatches
-source_list <- c("BirdLife", "eBird", "BirdTree")
-source_list_perms <- permn(source_list)
-total_mismatch <- c()
-for (i in 1:length(source_list_perms)){
-  test_id_list1 <- get_bird_ids(AVONET_df$Species1_BirdLife,
-                                source_list_perms[i])
-  test_id_list2 <- get_bird_ids(AVONET_df$Species2_eBird,
-                                source_list_perms[i])
-  test_id_list3 <- get_bird_ids(AVONET_df$Species3_BirdTree,
-                                source_list_perms[i])
-  
-  list1_mistmatch <- which(test_id_list1!=AVONET_df$Avibase.ID)
-  mismatch_species1 <- AVONET_df$Species1_BirdLife[list1_mistmatch]
-  list2_mistmatch <- which(test_id_list2!=AVONET_df$Avibase.ID)
-  mismatch_species2 <- AVONET_df$Species2_eBird[list2_mistmatch]
-  list3_mistmatch <- which(test_id_list3!=AVONET_df$Avibase.ID)
-  mismatch_species3 <- AVONET_df$Species3_BirdTree[list3_mistmatch]
-  total_mismatch <- append(total_mismatch,
-                           length(list1_mistmatch) + 
-                             length(list2_mistmatch) + 
-                             length(list3_mistmatch))
-}
+# Compare differing number of mismatches to differing numbers of species:
+cat("BirdLife has",
+    length(unique(AVONET_df$Species1_BirdLife))-
+      length(unique(AVONET_df$Species2_eBird)),
+    "more species than eBird, and eBird produces",
+    length(mismatch_species2) - length(mismatch_species1),
+    "more mismatches.")
+cat("BirdLife has",
+    length(unique(AVONET_df$Species1_BirdLife))-
+      length(unique(AVONET_df$Species3_BirdTree)),
+    "more species than BirdTree, and BirdTree produces",
+    length(mismatch_species3) - length(mismatch_species1),
+    "more mismatches.")
+# These values are close together, suggesting the number of mismatches is
+# closely related to the degree of splitting/lumping.
+# Try comparing directly with number of ID's instead:
+cat("BirdLife has",
+    length(unique(AVONET_df$Avibase.ID))-
+      length(unique(AVONET_df$Species1_BirdLife)),
+    "fewer species than AVIBASE, and produces",
+    length(mismatch_species1),
+    "mismatches.")
+cat("eBird has",
+    length(unique(AVONET_df$Avibase.ID))-
+      length(unique(AVONET_df$Species2_eBird)),
+    "fewer species than AVIBASE, and produces",
+    length(mismatch_species2),
+    "mismatches.")
+cat("BirdTree has",
+    length(unique(AVONET_df$Avibase.ID))-
+      length(unique(AVONET_df$Species3_BirdTree)),
+    "fewer species than AVIBASE, and produces",
+    length(mismatch_species3),
+    "mismatches.")
+# How does it compare to number of distinct combinations of ID and species name?
+cat("There are",
+    nrow(distinct(AVONET_df[, c("Avibase.ID", "Species1_BirdLife")])),
+    "distinct combinations of Avibase ID and BirdLife names, and",
+    nrow(AVONET_df)-length(mismatch_species1),
+    "successful matches.")
+cat("There are",
+    nrow(distinct(AVONET_df[, c("Avibase.ID", "Species2_eBird")])),
+    "distinct combinations of Avibase ID and BirdLife names, and",
+    nrow(AVONET_df)-length(mismatch_species2),
+    "successful matches.")
+cat("There are",
+    nrow(distinct(AVONET_df[, c("Avibase.ID", "Species3_BirdTree")])),
+    "distinct combinations of Avibase ID and BirdLife names, and",
+    nrow(AVONET_df)-length(mismatch_species3),
+    "successful matches.")
+
+length(unique(test_id_list1))
+length(unique(test_id_list2))
+length(unique(test_id_list3))
