b955ab96245407c8135a82cc049576cf64d832e8
Joe Hilton <joe.b.hilton@gmail.com>
2023-07-03 16:55:47 +0100

    Added verification and analysis of consensus trees

--- eco_phylo_processing/consensus_trees.R
+++ eco_phylo_processing/consensus_trees.R
@@ -47,5 +47,65 @@ cat("Expected execution time for 1000 trees is",
       "seconds.\n")
 }
 write.nexus(consensus_tree,
-            file = "data/phylogeny/consensus_bird_tree",
+            file = "data/phylogeny/consensus_bird_tree.nex",
             translate = FALSE)
+consensus_copy <- read.nexus("data/phylogeny/consensus_bird_tree.nex")
+
+# Check it writes and reads properly by working out distance matrices for both:
+dmat <- cophenetic.phylo(consensus_tree)
+dmat_copy <- cophenetic.phylo(consensus_copy)
+dmat_order <- dmat[, order(colnames(dmat))]
+dmat_order <- dmat_order[order(rownames(dmat)), ]
+dmat_copy_order <- dmat_copy[, order(colnames(dmat_copy))]
+dmat_copy_order <- dmat_copy_order[order(rownames(dmat_copy)), ]
+cat("Max difference in sorted distance matrices is",
+    max(dmat_order - dmat_copy_order),
+    ".\n")
+# The maximum difference is NaN, so there must be NaN's in one or both of the
+# distance matrices. What species do these correspond to?
+rows_to_inspect <- 1:50
+rownames(dmat_order)[which(is.nan(rowSums(dmat_order[rows_to_inspect, ])))]
+# Should find all the first fifty rows have a NaN somewhere!
+
+# Try identifying the species where we have NaN's:
+nan_sp_list <- list()
+for (sp in 1:5){
+  nan_sp <- colnames(dmat_order)[which(is.nan(dmat_order[sp, ]))]
+  cat("NaN distance between",
+      rownames(dmat_order)[sp],
+      "and",
+      nan_sp,
+      ".\n",
+      sep = "\n")
+  nan_sp_list <- append(nan_sp_list, list(nan_sp))
+}
+
+# Appears to be the same species consistently, so let's keep going:
+for (sp in 6:nrow(dmat_order)){
+  nan_sp <- colnames(dmat_order)[which(is.nan(dmat_order[sp, ]))]
+  nan_sp_list <- append(nan_sp_list, list(nan_sp))
+}
+
+# See if it's the same entries for all species:
+unique_nan_sp_list <- unique(nan_sp_list)
+
+# We should see that there's a list of 43 "bad species" that consistently show
+# up, plus 40 other lists consisting of all the species minus one or two.
+# To me this suggests that we have 39 species that don't link to everything plus
+# 4 that are in two 2-species clusters.
+
+sp_names <- rownames(dmat)
+n_samples <- 10
+sp_sample <- sample(sp_names, 2 * n_samples)
+dsamples <- c()
+dsamples_copy <- c()
+for (i in 1:n_samples){
+  sp1_idx <- which(rownames(dmat)==sp_sample[i])
+  sp2_idx <- which(rownames(dmat)==sp_sample[n_samples + i])
+  dsamples <- append(dsamples, dmat[sp1_idx, sp2_idx])
+  
+  sp1_idx <- which(rownames(dmat_copy)==sp_sample[i])
+  sp2_idx <- which(rownames(dmat_copy)==sp_sample[n_samples + i])
+  dsamples_copy <- append(dsamples_copy, dmat_copy[sp1_idx, sp2_idx])
+}
+identical(dsamples, dsamples_copy)
