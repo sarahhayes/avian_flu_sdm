609a644506363902f6eaee5e01dd451f6479dedf
sarahhayes <sarah.hayes16@imperial.ac.uk>
2023-07-14 12:18:41 +0100

    removing entries with wrong dates

--- avian_flu_scripts/prep_bvbrc.R
+++ avian_flu_scripts/prep_bvbrc.R
@@ -41,147 +41,19 @@ neg_data_europe %>%
   group_by(Collection.Country) %>%
   count()
 
+
+## there are two wrongly entered collection dates in the negative data.
+## Opt to remove 
+## Doing step-by-step to ensure removing just one at a time 
+
+nd <- neg_data_europe[which(neg_data_europe$Collection.Date != "0002-10-11T00:00:00Z"),] 
+nd <- nd[which(nd$Collection.Date != "0005-12-09T00:00:00Z"),]
+
+
 # Save these files 
 
 #write.csv(pos_data_europe, "data/flu_data/prepped_data/bvbrc_pos_europe.csv")
-#write.csv(neg_data_europe, "data/flu_data/prepped_data/bvbrc_neg_europe.csv")
-
-
-
-## there are two wrongly entered collection dates in the negative data.
-## Will initially change to what I think but might be better to remove? 
-
-neg_data_europe[which(neg_data_europe$Collection.Date == "0002-10-11T00:00:00Z"),
-                "Collection.Date"] <- "2002-10-11T00:00:00Z"
-
-neg_data_europe[which(neg_data_europe$Collection.Date == "0005-12-09T00:00:00Z"),
-                "Collection.Date"] <- "2005-12-09T00:00:00Z"
-
-
-
-
-
-bvbrc_subtype <- as.data.frame(table(pos_data_europe$Subtype))
-
-bvbrc_subtype_country <- as.data.frame(table(pos_data_europe$Collection.Country, 
-                                             pos_data_europe$Subtype)) %>%
-  filter(Freq != 0) %>%
-  filter(Var2 != "") %>%
-  filter(!Var2 %in% c("HxNx", "HxNy", "HXNX"))
-
-#write.csv(bvbrc_subtype, "output/bvbrc_subtypes.csv", row.names = F)
-#write.csv(bvbrc_subtype_country, "output/bvbrc_subtypes_countries.csv", row.names = F)
-
-# counts per country
-pos_data_europe_counts <- pos_data_europe %>%
-  dplyr::count(as.factor(Collection.Country),as.factor(Collection.Year),.drop=FALSE) %>%
-  rename(Country = "as.factor(Collection.Country)", Year = "as.factor(Collection.Year)")
-
-neg_data_europe_counts <- neg_data_europe %>%
-  count(as.factor(Collection.Country),as.factor(Collection.Year),.drop=FALSE) %>%
-  rename(Country = "as.factor(Collection.Country)", Year = "as.factor(Collection.Year)")
-
-
-ggplot(pos_data_europe_counts, aes(x = Year, y = n, 
-                                   fill = Country, colour = Country)) + 
-  geom_bar(stat = "identity", position = "dodge") + 
-  labs(x = "Year", y = "Frequency", title = "Positive samples from wild birds in Europe")
-
-
-ggplot(neg_data_europe_counts, aes(x = Year, y = n, 
-                                   fill = Country, colour = Country)) + 
-  geom_bar(stat = "identity", position = "dodge") + 
-  labs(x = "Year", y = "Frequency", title = "Negative samples from wild birds in Europe")
-
-
-## just by country and not by year
-pos_data_europe %>%
-  dplyr::count(as.factor(Collection.Country), .drop=FALSE) %>%
-  rename(Country = "as.factor(Collection.Country)") %>%
-  ggplot(., aes(x = Country, y = n, fill = Country, colour = Country)) + 
-  geom_bar(stat = "identity", position = "dodge") + 
-  labs(x = "Year", y = "Frequency", 
-       title = "Positive samples from wild birds in Europe 
-       (BV-BRC)") +
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1), legend.position = "none")
-#ggsave("plots/by_country_pos_bvbrc.png" )#, width = 5, height = 5)
-
-neg_data_europe %>%
-  dplyr::count(as.factor(Collection.Country), .drop=FALSE) %>%
-  rename(Country = "as.factor(Collection.Country)") %>%
-  ggplot(., aes(x = Country, y = n, fill = Country, colour = Country)) + 
-  geom_bar(stat = "identity", position = "dodge") + 
-  labs(x = "Year", y = "Frequency", title = "Negative samples from wild  
-       birds in Europe (BV-BRC)") +
-  theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust=1), legend.position = "none")
-# ggsave("plots/by_country_neg_bvbrc.png")
-
-
-## plots of time series of the data
-## plot a time series of the cases. 
-pos_data_europe$date <- strptime(pos_data_europe$Collection.Date,
-                                 format = "%Y-%m-%d")
-
-pos_data_europe$month_year <- format(pos_data_europe$date, format = "%m/%Y")
-pos_data_europe$month <- format(pos_data_europe$date, format = "%m")
-
-pos_counts <- pos_data_europe %>% group_by(month_year) %>% count()
-pos_counts$date <- as.Date(zoo::as.yearmon(pos_counts$month_year, "%m/%Y"))
-
-ggplot(pos_counts, aes(x = date, y = n)) + 
-  geom_line( col = "red", size = 1) + 
-  labs(title = "Positive cases in wild birds in Europe 
-       (BV-BRC)", 
-       x = "Date", y = "Count") + 
-  theme_bw() + 
-  xlim(c(as.Date("2003-01-01"), as.Date("2023-01-01")))
-
-#ggsave("plots/timeline_bvbrc_pos.png")
-
-## look at entries per month
-month_counts <- as.data.frame(table(pos_data_europe$month))
-month_counts$Var1 <- as.character(month_counts$Var1)
-ggplot(data = month_counts, aes(x = Var1, y = Freq)) + 
-  geom_bar(stat = "identity", col = "orange", fill = "orange") + 
-  labs(x = "Month", title = "Monthly positive counts bvbrc data") 
-
-
-# now negative
-neg_data_europe$date <- strptime(neg_data_europe$Collection.Date,
-                                 format = "%Y-%m-%d")
-
-neg_data_europe$month_year <- format(neg_data_europe$date, format = "%m/%Y")
-neg_data_europe$month <- format(neg_data_europe$date, format = "%m")
-
-
-neg_counts <- neg_data_europe %>% group_by(month_year) %>% count()
-neg_counts$date <- as.Date(zoo::as.yearmon(neg_counts$month_year, "%m/%Y"))
-
-# There are two dates that are wrong
-str(neg_data_europe)
-
-ggplot(neg_counts, aes(x = date, y = n)) + 
-  geom_line( col = "red", size = 1) + 
-  labs(title = "Negative cases in wild birds in Europe 
-       (BV-BRC)", 
-       x = "Date", y = "Count") + 
-  theme_bw() + 
-  xlim(c(as.Date("2003-01-01"), as.Date("2023-01-01")))
-
-#ggsave("plots/timeline_bvbrc_neg.png")
-
-# plot of monthly counts of negative cases
-month_counts_neg <- as.data.frame(table(neg_data_europe$month))
-month_counts_neg$Var1 <- as.character(month_counts_neg$Var1)
-ggplot(data = month_counts_neg, aes(x = Var1, y = Freq)) + 
-  geom_bar(stat = "identity", col = "orangered3", fill = "orangered3", ) + 
-  labs(x = "Month", title = "Monthly negative counts bvbrc data")
-
-
-### Look at the species that are included in the data 
-
-species_table <- as.data.frame(table(pos_data_europe$Host.Species))
-#write.csv(species_table, "output/species_bvbrc_pos.csv")
+#write.csv(nd, "data/flu_data/prepped_data/bvbrc_neg_europe.csv")
 
 
 
