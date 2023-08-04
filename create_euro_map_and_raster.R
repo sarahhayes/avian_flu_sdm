6d20da690752d4ca2ca45e46dbb62bfde6ed1c37
sarahhayes <sarah.hayes16@imperial.ac.uk>
2023-08-04 10:15:33 +0100

    updates to distance to coast to remove issue with map border

--- create_euro_map_and_raster.R
+++ create_euro_map_and_raster.R
@@ -15,6 +15,7 @@ plot(zipmap)
 # change projection and crop 
 crs <- "epsg:3035"
 euro_ext <- terra::ext(2600000, 7000000, 1500000, 6400000) 
+large_ext <- terra::ext(2000000, 9000000, 1000000, 7000000)
 
 # using a blank raster doesn't seem to work with the vector so do step by step 
 euro_map_crop <- terra::project(x = zipmap, y = crs) 
@@ -42,3 +43,14 @@ blank_10k <- terra::rast(crs=crs, extent=euro_ext, res = 10000)
 eurorast_10k <- rasterize(euro_map_crop_prj, blank_10k)
 plot(eurorast_10k)
 # writeRaster(eurorast_10k, "output/euro_rast_10k.tif", overwrite = T)
+
+# larger shapefile for use in distance to couast
+large_ext <- terra::ext(1800000, 9000000, 1000000, 7200000)
+
+# using a blank raster doesn't seem to work with the vector so do step by step 
+large_map_crop <- terra::project(x = zipmap, y = crs) 
+large_map_crop_prj <- terra::crop(large_map_crop, large_ext) 
+plot(large_map_crop_prj)
+plot(euro_map_crop_prj, col = "red", add = T, axes = F)
+
+# writeVector(large_map_crop_prj, "output/large_map.shp", overwrite = T)
