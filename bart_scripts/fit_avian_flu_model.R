f7145eb3c1b893da4c1f412a74102a464f593cb2
Joe Hilton <joe.b.hilton@gmail.com>
2023-08-17 14:52:27 +0100

    Wrote fits/plots for BART

--- bart_scripts/fit_avian_flu_model.R
+++ bart_scripts/fit_avian_flu_model.R
@@ -22,8 +22,8 @@ eco_lyrnames <- eco_paths %>%
 
 eco_layers <- rast(lapply(eco_paths, rast))
 
-# Restrict to quarter 1:
-eco_layers <- eco_layers[[seq(1, nlyr(eco_layers), 4)]]
+# Restrict to quarter 2:
+eco_layers <- eco_layers[[seq(2, nlyr(eco_layers), 4)]]
 set.names(eco_layers, eco_lyrnames)
 
 # Now do environmental:
@@ -31,7 +31,7 @@ env_paths <- list.files("../../../OneDrive - The University of Liverpool/AI_S2_S
                         pattern = "*.tif",
                         full.names = TRUE)
 env_paths <- env_paths[-grep("landcover_type1_full_raster", env_paths)]
-env_paths <- env_paths[-grep("second_quart", env_paths)]
+env_paths <- env_paths[-grep("first_quart", env_paths)]
 env_paths <- env_paths[-grep("third_quart", env_paths)]
 env_paths <- env_paths[-grep("fourth_quart", env_paths)]
 env_lyrnames <- env_paths %>%
@@ -41,5 +41,80 @@ env_lyrnames <- env_paths %>%
       replacement = "")
 
 env_layers <- rast(lapply(env_paths, rast))
+names(env_layers) <- gsub("6_Ch_2015_Aw", "chicken_density", names(env_layers))
+names(env_layers) <- gsub("6_Dk_2015_Aw", "duck_density", names(env_layers))
+names(env_layers) <- gsub("mean", "ndvi_second_quart", names(env_layers))
 
-first_quart_rast <- c(resample(eco_layers, env_layers), env_layers)
+cov_coast <- read.csv("../../../OneDrive - The University of Liverpool/AI_S2_SDM_storage/Environmental variable csvs/dist_to_coast_output.csv") %>%
+  rename(x = X, y = Y) %>%
+  select(-X.1) %>% 
+  relocate(x,y) %>%
+  rast(type = "xyz", crs = "epsg:3035")
+cov_water <- read.csv("../../../OneDrive - The University of Liverpool/AI_S2_SDM_storage/Environmental variable csvs/dist_to_water_output.csv") %>%
+  select(-ID) %>% 
+  relocate(x,y) %>%
+  rast(type = "xyz", crs = "epsg:3035")
+cov_alt <- read.csv("../../../OneDrive - The University of Liverpool/AI_S2_SDM_storage/Environmental variable csvs/elevation_outputs.csv") %>%
+  select(-ID, -X) %>% 
+  relocate(x,y) %>%
+  rast(type = "xyz", crs = "epsg:3035")
+
+landcover_rast <- rast("../../../OneDrive - The University of Liverpool/AI_S2_SDM_storage/Environmental rasters/landcover_type1_full_raster.tif") %>%
+                  resample(env_layers)
+crs(landcover_rast) <- crs
+
+second_quart_covs <- c(resample(eco_layers, env_layers),
+                       landcover_rast,
+                       env_layers,
+                       cov_coast,
+                       cov_water,
+                       cov_alt)
+rm(eco_layers,
+   landcover_rast,
+   env_layers,
+   cov_coast,
+   cov_water,
+   cov_alt)
+
+writeRaster(second_quart_covs, "../../../OneDrive - The University of Liverpool/AI_S2_SDM_storage/quarterly_covariates/second_quart_covs.tif", overwrite = TRUE)
+covstack <- raster::stack("../../../OneDrive - The University of Liverpool/AI_S2_SDM_storage/quarterly_covariates/second_quart_covs.tif")
+
+# Load training data
+training_coords <- readRDS("training_sets/training_coords_Q2.RDS")
+cov_df <- raster::extract(second_quart_covs, training_coords[, 1:2])
+cov_df <- cov_df[, -1]
+
+# Initialise model
+basic_model <- bart(cov_df,
+                    training_coords$pos,
+                    xtest,
+                    keeptrees = TRUE)
+
+sdm <- bart.step(x.data = cov_df,
+                 y.data = training_coords$pos,
+                 full = TRUE,
+                 quiet = TRUE)
+
+euro_10k <- rast("output/euro_rast_10k.tif")
+covstack_lores <- aggregate(covstack, fact = 10)
+
+# Generate risk map
+pred_layer <- predict(object = sdm,
+                      x.layers = covstack_lores,
+                      quantiles = c(0.025, 0.975),
+                      splitby = 20
+                      )
+
+# Plot map
+plot(pred_layer[[1]]>0.4,
+     box = FALSE,
+     axes = FALSE,
+     main = 'Avian flu risk')
+plot(pred_layer[[2]]>0.4,
+     box = FALSE,
+     axes = FALSE,
+     main = '2.5% posterior bound')
+plot(pred_layer[[3]]>0.4,
+     box = FALSE,
+     axes = FALSE,
+     main = '97.5% posterior bound')
