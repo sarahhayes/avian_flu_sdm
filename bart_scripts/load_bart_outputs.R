70bd89b1f676721be076d1a2ac79bc3f8d38b010
Joe Hilton <joe.b.hilton@gmail.com>
2023-08-24 14:21:44 +0100

    Added ROC plot script

--- bart_scripts/load_bart_outputs.R
+++ bart_scripts/load_bart_outputs.R
@@ -21,6 +21,26 @@ get_threshold <- function(object){
 }
 
 ################################################################################
+# Lookup table for renaming landcover layers
+lc_lookup <- pairlist("lc_1" = "Water_bodies",
+                      "lc_2" = "Evergreen_Needleleaf_Forests",
+                      "lc_3" = "Evergreen_Broadleaf_Forests",
+                      "lc_4" = "Deciduous_Needleleaf_Forests",
+                      "lc_5" = "Deciduous_Broadleaf_Forests",
+                      "lc_6" = "Mixed_Forests",
+                      "lc_7" = "Closed_Shrublands",
+                      "lc_8" = "Open_Shrublands",
+                      "lc_9" = "Woody_Savannas",
+                      "lc_10" = "Savannas",
+                      "lc_11" = "Grasslands",
+                      "lc_12" = "Permanent_Wetlands",
+                      "lc_13" = "Croplands",
+                      "lc_14" = "Urband_and_Built-up_Lands",
+                      "lc_15" = "Cropland/Natural_Vegetation_Mosaics",
+                      "lc_16" = "Non-Vegetated_Lands",
+                      "lc_17" = "Unclassified")
+
+################################################################################
 # Example: load in Q1 stuff
 
 # If you want to get covariates as a raster, run this with the appropriate address:
@@ -29,6 +49,12 @@ get_threshold <- function(object){
 # Load fitted and optimised model:
 load("output/fitted-BART-models/sdm_Q1.rds")
 xtrain <- sdm$fit$data@x
+colnames(xtrain) <- lapply(colnames(xtrain),
+                                  FUN = function(n){
+                                    if (n %in% names(lc_lookup)){
+                                      n <- lc_lookup[[n]]}else{
+                                        n <- n}
+                                      })
 ytrain <- sdm$fit$data@y
 xtest <- sdm$fit$data@x.test
 
---
+++ bart_scripts/plot_roc.R
@@ -0,0 +1,110 @@
+# In this script we load in the outputs from fit_avian_flu_model.R.
+
+library(embarcadero)
+library(ggplot2)
+library(raster)
+library(RColorBrewer)
+library(terra)
+set.seed(12345)
+
+# Function for directly getting optimal cutoff
+get_threshold <- function(object){
+  fitobj <- object$fit
+  
+  true.vector <- fitobj$data@y 
+  
+  pred <- prediction(colMeans(pnorm(object$yhat.train)), true.vector)
+  
+  perf.tss <- performance(pred,"sens","spec")
+  tss.list <- (perf.tss@x.values[[1]] + perf.tss@y.values[[1]] - 1)
+  tss.df <- data.frame(alpha=perf.tss@alpha.values[[1]],tss=tss.list)
+  thresh <- min(tss.df$alpha[which(tss.df$tss==max(tss.df$tss))])
+  return(thresh)
+}
+
+################################################################################
+# Lookup table for renaming landcover layers
+lc_lookup <- pairlist("lc_1" = "Water_bodies",
+                      "lc_2" = "Evergreen_Needleleaf_Forests",
+                      "lc_3" = "Evergreen_Broadleaf_Forests",
+                      "lc_4" = "Deciduous_Needleleaf_Forests",
+                      "lc_5" = "Deciduous_Broadleaf_Forests",
+                      "lc_6" = "Mixed_Forests",
+                      "lc_7" = "Closed_Shrublands",
+                      "lc_8" = "Open_Shrublands",
+                      "lc_9" = "Woody_Savannas",
+                      "lc_10" = "Savannas",
+                      "lc_11" = "Grasslands",
+                      "lc_12" = "Permanent_Wetlands",
+                      "lc_13" = "Croplands",
+                      "lc_14" = "Urband_and_Built-up_Lands",
+                      "lc_15" = "Cropland/Natural_Vegetation_Mosaics",
+                      "lc_16" = "Non-Vegetated_Lands",
+                      "lc_17" = "Unclassified")
+
+################################################################################
+# Make AUC plot and getting numbers for table
+
+# Load models:
+load("output/fitted-BART-models/sdm_Q1.rds")
+q1_sdm <- sdm
+load("output/fitted-BART-models/sdm_Q2.rds")
+q2_sdm <- sdm
+load("output/fitted-BART-models/sdm_Q3.rds")
+q3_sdm <- sdm
+load("output/fitted-BART-models/sdm_Q4.rds")
+q4_sdm <- sdm
+rm(sdm)
+
+get_sens_and_spec <- function(sdm, cutoff){
+  pred <- sdm$yhat.train %>% pnorm() %>%
+    colMeans() %>%
+    prediction(labels = sdm$fit$data@y)
+  perf <-  performance(q1_pred, measure = "sens", x.measure = "spec")
+  sens <- perf@x.values[[1]][which(perf@alpha.values[[1]]==cutoff)]
+  spec <- perf@y.values[[1]][which(perf@alpha.values[[1]]==cutoff)]
+  return(pairlist("pred"=pred,
+                  "perf"=perf,
+                  "sens"=sens,
+                  "spec"=spec))
+}
+
+# Try getting sensitivity and specificity:
+q1_cutoff <- get_threshold(q1_sdm)
+q2_cutoff <- get_threshold(q2_sdm)
+q3_cutoff <- get_threshold(q3_sdm)
+q4_cutoff <- get_threshold(q4_sdm)
+
+q1_sens_spec <- get_sens_and_spec(q1_sdm, q1_cutoff)
+q2_sens_spec <- get_sens_and_spec(q2_sdm, q2_cutoff)
+q3_sens_spec <- get_sens_and_spec(q3_sdm, q3_cutoff)
+q4_sens_spec <- get_sens_and_spec(q4_sdm, q4_cutoff)
+
+q1_x <- performance(q1_sens_spec$pred, "tpr", "fpr")
+q2_x <- performance(q2_sens_spec$pred, "tpr", "fpr")
+q3_x <- performance(q3_sens_spec$pred, "tpr", "fpr")
+q4_x <- performance(q4_sens_spec$pred, "tpr", "fpr")
+q1_rocdf <- data.frame(fpr=q1_x@x.values[[1]],
+                    tpr=q1_x@y.values[[1]])
+q2_rocdf <- data.frame(fpr=q2_x@x.values[[1]],
+                       tpr=q2_x@y.values[[1]])
+q3_rocdf <- data.frame(fpr=q3_x@x.values[[1]],
+                       tpr=q3_x@y.values[[1]])
+q4_rocdf <- data.frame(fpr=q4_x@x.values[[1]],
+                       tpr=q4_x@y.values[[1]])
+pal <- c("#2271B2",
+         "#F748A5",
+         "#359B73",
+         "#e69f00")
+roc_plot <- ggplot() + 
+  geom_line(aes(x=q1_rocdf$fpr,y=q1_rocdf$tpr, colour = "Q1")) + 
+  geom_line(aes(x=q2_rocdf$fpr,y=q2_rocdf$tpr, colour = "Q2")) + 
+  geom_line(aes(x=q3_rocdf$fpr,y=q3_rocdf$tpr, colour = "Q3")) + 
+  geom_line(aes(x=q4_rocdf$fpr,y=q4_rocdf$tpr, colour = "Q4")) + 
+  scale_colour_manual("",
+                      breaks = c("Q1", "Q2", "Q3", "Q4"),
+                      values = pal) +
+  ggtitle('Receiver-operator curve') + 
+  xlab('False positive rate') + 
+  ylab('True positive rate') + 
+  geom_abline(intercept=0,slope=1,col='black')
