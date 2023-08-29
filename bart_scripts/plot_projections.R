4ffcbb6fadefc5100c7c0ce7c613ef1191eb1e50
Joe Hilton <joe.b.hilton@gmail.com>
2023-08-29 12:48:05 +0100

    Added projection plotting script

---
+++ bart_scripts/plot_projections.R
@@ -0,0 +1,22 @@
+# In this script we plot risk projections using the outputs from
+# fit_avian_flu_model.R.
+
+library(embarcadero)
+library(raster)
+library(terra)
+library(viridis)
+set.seed(12345)
+
+for (idx in 1:4){
+  load(file = paste("output/fitted-BART-models/prediction_Q",
+                    idx,
+                    ".rds",
+                    sep = ""))
+       png(filename = paste("plots/q", idx, "_poster.png", sep = ""))
+  plot(pred_layer[[1]],
+       col = viridis_pal()(100),
+       main = paste("Q", idx, sep = ""),
+       legend.args = list(text = "Probability", side = 2))
+  dev.off()
+  
+}
