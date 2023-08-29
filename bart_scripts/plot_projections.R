5b6a9ec2c3a2d2ebe0614966028941005e1d75f9
Liam Brierley <liam.brierley@liverpool.ac.uk>
2023-08-29 16:13:06 +0100

    update poster predictions plot

--- bart_scripts/plot_projections.R
+++ bart_scripts/plot_projections.R
@@ -1,24 +1,36 @@
 # In this script we plot risk projections using the outputs from
 # fit_avian_flu_model.R.
 
+rm(list = ls())
+
 library(embarcadero)
 library(raster)
 library(terra)
 library(viridis)
 set.seed(12345)
 
+preds <- list()
+
+
 for (idx in 1:4){
   load(file = paste("output/fitted-BART-models/prediction_Q",
                     idx,
                     ".rds",
                     sep = ""))
-       png(filename = paste("plots/q", idx, "_poster.png", sep = ""))
-  plot(pred_layer[[1]],
-       box = FALSE,
-       axes = FALSE,
-       col = viridis_pal()(100),
-       main = paste("Q", idx, sep = ""),
-       legend.args = list(text = "Probability", side = 2))
-  dev.off()
+  preds[[idx]] <- pred_layer[[1]]
+  names(preds[[idx]]) <- paste0("Q",idx)
   
 }
+
+png("plots/all_poster.png", width = 7, height = 7,
+    units = "in", res = 330)
+spplot(stack(preds),
+       col.regions = viridis_pal()(100),
+       at = seq(0,1,0.01),
+       cex = 0.8)
+grid::grid.text("Probability", x=grid::unit(0.98, "npc"), y=grid::unit(0.50, "npc"), rot=-90)
+dev.off()
+
+
+
+
