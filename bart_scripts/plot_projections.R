071685b7592cb938cc4990a5d4be6ef3ed6f3e1a
Liam Brierley <liam.brierley@liverpool.ac.uk>
2023-08-30 13:34:46 +0100

    add PDF of 95% credible intervals for quarterly predictions

--- bart_scripts/plot_projections.R
+++ bart_scripts/plot_projections.R
@@ -17,20 +17,44 @@ for (idx in 1:4){
                     idx,
                     ".rds",
                     sep = ""))
-  preds[[idx]] <- pred_layer[[1]]
-  names(preds[[idx]]) <- paste0("Q",idx)
+  preds[[idx]] <- pred_layer
+  names(preds[[idx]]) <- c(paste0("Q",idx),
+                           paste0("Q",idx,"_2.5th_percentile"),
+                           paste0("Q",idx,"_97.5th_percentile"))
   
 }
 
 png("plots/all_poster.png", width = 7, height = 7,
     units = "in", res = 330)
-spplot(stack(preds),
+spplot(stack(preds[[1]][[1]],
+             preds[[2]][[1]], 
+             preds[[3]][[1]], 
+             preds[[4]][[1]]),
        col.regions = viridis_pal()(100),
        at = seq(0,1,0.01),
        cex = 0.8)
 grid::grid.text("Probability", x=grid::unit(0.98, "npc"), y=grid::unit(0.50, "npc"), rot=-90)
 dev.off()
 
+
+pdf("plots/all_uncertainty.pdf", paper="a4", width = 8, height = 11.3)
+spplot(stack(preds[[1]][[2]],
+             preds[[1]][[1]],
+             preds[[1]][[3]],
+             preds[[2]][[2]], 
+             preds[[2]][[1]], 
+             preds[[2]][[3]], 
+             preds[[3]][[2]], 
+             preds[[3]][[1]], 
+             preds[[3]][[3]], 
+             preds[[4]][[2]],
+             preds[[4]][[1]],
+             preds[[4]][[3]]),
+       col.regions = viridis_pal()(100),
+       at = seq(0,1,0.01),
+       cex = 0.8)
+grid::grid.text("Probability", x=grid::unit(0.98, "npc"), y=grid::unit(0.50, "npc"), rot=-90)
+dev.off()
 
 
 
